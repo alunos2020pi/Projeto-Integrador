6a05a998e4061bff4d63de24e21aa117f456e622
Marcos Pereira da Silva Cruz <marcospcruz@gmail.com>
2021-10-08 16:15:55 -0300

    URLs.py configurada para direcionar para views.index, criação da função index em views.py, configuração de templates e acréscimo da página index.html com contadores de supermercados, produtos e ofertas

--- ofertas/urls.py
+++ ofertas/urls.py
@@ -3,5 +3,5 @@ from ofertas import views
 
 
 urlpatterns = [
-
+    path('', views.index, name='index'),
 ]
--- ofertas/views.py
+++ ofertas/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from ofertas.models import Supermercado, Produto, Em_Oferta
+
+def index(request):
+    """Função view para a página inicial do site"""
+    #Gera alguns contadores para os objetos principais
+    num_super = Supermercado.objects.all().count()
+    num_prod = Produto.objects.all().count()
+    num_ofertas = Em_Oferta.objects.all().count()
+    
+    
+    context = {
+        'num_super': num_super,
+        'num_prod': num_prod,
+        'num_ofertas': num_ofertas,
+    }
+    
+    #Renderiza o template index.html com os dados na variável context
+    return render(request, 'index.html', context=context)
+    
