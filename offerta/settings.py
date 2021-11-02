2eb40f0550f7d7986dd3313d967636533b6797f4
Marcos Pereira da Silva Cruz <marcospcruz@gmail.com>
2021-11-02 18:38:13 -0300

    Movi as pastas static e templates de volta onde estavam e escrevi algo no Sobre Nós

--- offerta/settings.py
+++ offerta/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'put your secret key here'
+#SECRET_KEY = 'put your secret key here'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'put your secret key here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
