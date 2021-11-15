a3fba263fec9a3739e27d17660e98348b6a624ed
Marcos Pereira da Silva Cruz <marcospcruz@gmail.com>
2021-11-15 13:10:50 -0300

    Atualizado ALLOWED_HOSTS com offertas2.herokuapp.com

--- offerta/settings.py
+++ offerta/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'put your secret key here')
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','offertas.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','offertas.herokuapp.com','offertas2.herokuapp.com']
 
 
 # Application definition
